22c5b723217b94ec774f88e2a1adc9b8f13317c0
kauedesousa <kaue.desousa@inn.no>
2020-08-10 13:23:22 +0200

    updtae-head

--- ClimMob.R
+++ ClimMob.R
@@ -6,7 +6,7 @@
 
 # tag <- "CR2020"
 # args <- c(paste0("dev/data/",tag,"/data.json"), paste0("dev/data/",tag,"/info.json"),
-#          paste0("dev/output/",tag,"/"), "TRUE","en","docx",
+#          paste0("dev/output/",tag,"/"), "TRUE","en","html",
 #          "participant", "option", getwd(), NA)
 
 # get the arguments from server's call
