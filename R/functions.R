04c19006a612e2c7194732ecbbf0177b3a43e3aa
kauedesousa <kaue.desousa@inn.no>
2021-11-05 09:28:30 +0100

    add-source2

--- R/functions.R
+++ R/functions.R
@@ -1,19 +1,3 @@
-###Functions for Climmob Reporting Analysis
-
-
-#' Validate the class of objects generated in the tryCatch(s)
-any_error <- function(x){
-  isTRUE("error" %in% class(x))
-}
-
-#' Runs specific lines of the code
-source2 <- function(file, start, end, ...) {
-  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
-  file.lines.collapsed <- paste(file.lines, collapse='\n')
-  source(textConnection(file.lines.collapsed), ...)
-}
-
-
 #' Plot map using leaflet
 #' @param data a data frame
 #' @param xy index of data for the longitude and latitude coordinates (in that order)
@@ -797,5 +781,17 @@ rename_duplicates <- function(x, rename.with = "numbers", sep = "") {
   
   return(x)
   
+}
+
+#' Validate the class of objects generated in the tryCatch(s)
+any_error <- function(x){
+  isTRUE("error" %in% class(x))
+}
+
+#' Runs specific lines of the code
+source2 <- function(file, start, end, ...) {
+  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
+  file.lines.collapsed <- paste(file.lines, collapse='\n')
+  source(textConnection(file.lines.collapsed), ...)
 }
 
