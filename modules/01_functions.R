9196d8e83356eef6b53091379a2238a9d8223c76
kauedesousa <kaue.desousa@inn.no>
2024-03-17 17:41:09 +0100

    Update 01_functions.R

--- modules/01_functions.R
+++ modules/01_functions.R
@@ -1143,4 +1143,70 @@ forward_selection = function(data, ...) {
 
 
 
+#' Plot PCA 
+#' @param data an object of class princomp
+#' @param scale integer for the scale to be used in the vectors
+#' @param ... additional arguments passed to methods 
+plot_pca = function(object, scale = 3){
+  
+  sums = summary(object)
+  
+  vars = sums$sdev^2
+  
+  vars = round(vars/sum(vars) * 100, 1)
+  
+  pcd = as.data.frame(object$scores[,1:2])
+  names(pcd) = c("dim1", "dim2")
+  pcd$item = coeffs$item
+  
+  pcd_text = split(pcd, pcd$item)
+  
+  pcd_text = lapply(pcd_text, function(x){
+    data.frame(item = x$item[1],
+               dim1_m = mean(x$dim1),
+               dim2_m = mean(x$dim2))
+  })
+  
+  pcd_text = do.call("rbind", pcd_text)
+  
+  loadings = as.data.frame(object$loadings[1:length(traitlabels), ])
+  names(loadings) = paste0("dim", 1:ncol(loadings))
+  loadings$traits = rownames(loadings)
+  
+  pcplot = 
+    ggplot(pcd) +
+    geom_point(aes(x = dim1, y = dim2, color = item)) +
+    geom_segment(data = loadings, aes(x = 0, 
+                                      y = 0, 
+                                      xend = dim1 * scale,
+                                      yend = dim2 * scale),
+                 arrow = arrow(length = unit(0.3, "cm"), 
+                               type = "open", angle = 25),
+                 linewidth = 0.7, color = "grey20") +
+    geom_label_repel(data = pcd_text, aes(x = dim1_m,
+                                          y = dim2_m,
+                                          label = item,
+                                          color = item)) +
+    geom_text_repel(data = loadings,
+                    aes(label = traits,
+                        x = dim1 * scale,
+                        y = dim2 * scale),
+                    box.padding = 0.2,
+                    point.padding = 0.3,
+                    size = 3.5,
+                    color = "grey20", 
+                    arrow = arrow(length = unit(0.3, "cm"), 
+                                  type = "closed",
+                                  angle = 25),
+                    force = 4) +
+    theme_bw() +
+    theme(legend.position = "none") +
+    labs(x = paste0("Comp.1 (", vars[1], "%)"),
+         y = paste0("Comp.2 (", vars[2], "%)"))
+  
+  pcplot
+  
+}
+
+
 
